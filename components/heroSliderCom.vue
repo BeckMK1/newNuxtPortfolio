887ba6f65e645eda7e5c6fb7f46c9629cb389c79
Mikkel Beck Knudsen <47305516+BeckMK1@users.noreply.github.com>
2024-06-26 12:28:40 +0200

    slider almost done

--- components/heroSliderCom.vue
+++ components/heroSliderCom.vue
@@ -2,11 +2,25 @@
     <div class="heroSlider">
         <div class="sliderContainer">
             <div class="slider">
-                <div class="sliderBtn" @click="prevSlide"><font-awesome-icon icon="fa-solid fa-chevron-left" /></div>
+                <div class="sliderBtn prevBtn" @click="prevSlide"><font-awesome-icon icon="fa-solid fa-chevron-left" /></div>
                 <div class="slide" v-show="currentSlide == index" v-for="(slide, index) in slides" >
                     <img :src="slide.image">
+                    <div class="slideContent">
+                        <div class="textContent">
+                            <h3>{{ slide.title }}</h3>
+                            <p>{{ slide.content }}</p>
+                            <div class="techContainer">
+                                <p v-for="tech in slide.tech">{{ tech }}</p>
+                            </div>
+                        </div>
+                        <div class="links">
+                            <a class="projectLink" v-if="slide.githubLink != ''" :href="slide.githubLink"><font-awesome-icon icon="fa-brands fa-github" /></a>
+                            <a class="projectLink" v-if="slide.githubLink != ''" :href="slide.webLink"><font-awesome-icon icon="fa-solid fa-globe" /></a>
+                            <a class="projectLink" v-if="slide.githubLink != ''" :href="`projects#${slide.projectId}`"><font-awesome-icon icon="fa-solid fa-diagram-project" /></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="sliderBtn" @click="nextSlide"><font-awesome-icon icon="fa-solid fa-chevron-right" /></div>
+                <div class="sliderBtn nextBtn" @click="nextSlide"><font-awesome-icon icon="fa-solid fa-chevron-right" /></div>
             </div>
         </div>
     </div>
@@ -37,31 +51,75 @@ function prevSlide(){
 <style lang="scss" scoped>
     .heroSlider{
         max-height: 100vh;
-        max-width: 100vw;
+        max-width: 100%;
         height: 100vh;
+    }
+    .sliderContainer{
+        height: inherit;
     }
     .slider{
         position: relative;
         display: flex;
         justify-content: space-between;
         align-items: center;
+        height: 100%;
         .sliderBtn{
             position: relative;
             z-index: 2;
+            &.prevBtn{
+                margin-left: 3rem;
+            }
+            &.nextBtn{
+                margin-right: 3rem;
+            }
             svg{
                 color: white;
+                font-size: 2rem;
             }
         }
     }
     .slide{
-        height: 100vh;
         position: absolute;
         z-index: 0;
+        height: 100%;
+        width: 100%;
         img{
-            width: 100vw;
+            &::before{
+                content: "";
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                z-index: 1;
+                background-color: hsla(0,0%,0%,0.5);
+                top:0;
+                left: 0;
+            }
+            position: absolute;
+            z-index: 0;
+            width: 100%;
             height: 100vh;
             object-fit: cover;
             box-sizing: border-box;
+        }
+        .slideContent{
+            position: relative;
+            z-index: 2;
+            width: 80%;
+            margin: 10rem auto;
+            h3{
+                font-size: var(--fontXl);
+            }
+        }
+        .textContent{
+            .techContainer{
+                display: flex;
+                gap: 0.5rem;
+                p{
+                    padding: 0.25rem 1rem 0.33rem 1rem;
+                    border-radius: var(--radiusMd);
+                    background-color: var(--secondaryBg);
+                }
+            }
         }
     }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,24 @@
 <script setup>
 const slides = ref([
 {
-    image: "/images/slides/testImage1.jpg"
-}
+    image: "/images/slides/testImage1.jpg",
+    githubLink:"",
+    webLink:"",
+    projectId:"",
+    title:"Placeholder",
+    content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qua tu etiam inprudens utebare non numquam. Equidem, sed audistine modo de Carneade?",
+    tech:[
+        "Nuxt",
+        "MongoDB",
+        "Node.js",
+        "Express.js",
+        "Mongoose.js",
+        "Pinia",
+        "Fontawsome"
+
+    ]
+
+},
 ])
 </script>
 <style lang="scss" scoped></style>
