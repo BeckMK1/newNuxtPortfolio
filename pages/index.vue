4e23e3c43045976b9aa2051721734ad0060056ba
Mikkel Beck Knudsen <47305516+BeckMK1@users.noreply.github.com>
2024-08-09 12:07:26 +0200

    added text to the front page

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
     <div>
         <HeroSliderCom :slides="slides"></HeroSliderCom>
         <section id="quickAboutMe" class="mainPageContent"> 
-            <h3>Placeholder title</h3>
-            <p>sum dolor sit amet, consectetur adipiscing elit. Non autem hoc: igitur ne illud quidem. De quibus cupio scire quid sentias. Ita nemo beato beatior. Rationis enim perfectio est virtus; </p>
-            <NuxtLink class="linkBtn" to="/aboutMe">Next</NuxtLink>
+            <h3>Who am I?</h3>
+            <p><strong>My name is Mikkel Beck Knudsen,</strong> I am a webdeveloper who graduated as a Multimedia designer from <a href="https://www.eaaa.dk/" target="_blank">Aarhus erhvervs akademi</a> and I also graduated as a Multimedia integrator from <a href="https://videndjurs.dk/" target="_blank">Viden Djurs</a> in Grenaa.
+            I love making and learning new things and I have worked with a diverse set of tools such as: Nuxt, Umbraco, Drupal, WordPress, Node.js, MongoDB, Firebase and i am always looking to expand what I know. 
+            </p>
+            <NuxtLink class="linkBtn" to="/aboutMe">Read more</NuxtLink>
         </section>
     </div>
 </template>
@@ -51,5 +53,9 @@ const slides = ref([
         p{
             width: 100%;
         }
+    }
+    p a{
+        color: white;
+        font-weight: bold;
     }
 </style>
